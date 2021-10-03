4c5dbec3bf67667341dc8150c2d00afe87e5809f
michel <michelschaffers@gmail.com>
2021-10-03 15:52:24 +0200

    vue playground

---
+++ playgroundvue/test2/hellomichel/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HelloWorld msg="Welcome to Truc Vue.js App"/>
+  <ToDo todo="A faire pour michel"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ToDo from './components/ToDo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ToDo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ playgroundvue/test2/hellomichel/src/components/ToDo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="todo">
+    <h3>Todo component</h3>
+    <li>Todo: {{ todo }}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDo',
+  props: {
+    todo: String
+  }
+}
+</script>
