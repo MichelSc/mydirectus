ce3a9c1204b971551063709bac78c52ec5316d2a
Michel Schaffers <michel.schaffers@flandersmake.be>
2021-10-07 09:39:27 +0200

    Downgrade to vue 2

---
+++ playgroundvue/test3/testproj/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>Hello component</h1>
+    Message:{{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
